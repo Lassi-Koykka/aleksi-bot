7f4b824d5578142edf42b5d085673ad17ae4968c
Lassi Köykkä <lassi_koykka@hotmail.com>
2021-02-01 22:47:41 +0200

    Added automatic creation of the videos folder if one does not exist.

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const play = require('./play')
 const path = require('path');
 
 const client = new Discord.Client();
-const files = fs.readdirSync(path.join(__dirname, 'videos')).map(file => { return file.split('.')[0] })
+
+const videoDir = path.join(__dirname, 'videos');
+//Create dir for videos directory if it doesn't already exist;
+if(!fs.existsSync(videoDir)) {
+    fs.mkdirSync(videoDir);
+    console.log(`Video directory did not exist so one was created at: ${videoDir}\n`+ 
+    "Add the videoFiles you want available there.")
+} 
+const files = fs.readdirSync(videoDir).map(file => { return file.split('.')[0] })
 
 const { prefix, token } = require('./config.json');
 
@@ -24,8 +32,10 @@ client.on('message', async message => {
 
     //Get the first argument as command
     const commandName = args.shift().toLowerCase();
-    const filePath = path.join(__dirname, 'videos', commandName + '.mp4')
+    const filePath = path.join(videoDir, commandName + '.mp4')
 
+    // HELP: List available commands
+    // Or play the file with the same name as the command if it exists
     if (commandName === 'help') {
         let msgStr = "***Available commands:***\n``" + files.join("\n") + "``";
         message.channel.send(msgStr);
