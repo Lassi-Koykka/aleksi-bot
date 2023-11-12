70fae68a4384b93dffb83dd8727658b47614e28a
Lassi Köykkä <lassi_koykka@hotmail.com>
2023-11-12 23:03:07 +0200

    Update aleksi-bot to new discord.js and api version

--- index.js
+++ index.js
@@ -1,48 +1,71 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-const play = require('./play')
-const path = require('path');
+import play from "./play.js";
+import { ChannelType, Client, Events, GatewayIntentBits } from "discord.js";
+import { existsSync, mkdirSync, readdirSync } from "fs";
+import { join } from "path";
+import { default as dotenv } from "dotenv";
+import { getVoiceConnections } from "@discordjs/voice";
+dotenv.config();
 
-const client = new Discord.Client();
+const prefix = "aleksi";
+const TOKEN = process.env.DISCORD_BOT_TOKEN;
+if (!TOKEN) throw Error("DISCORD_BOT_TOKEN not found");
 
-const audioDir = path.join(__dirname, 'audio');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageTyping,
+    GatewayIntentBits.GuildVoiceStates,
+  ],
+});
+
+const audioDir = join(process.cwd(), "audio");
 //Create dir for audio directory if it doesn't already exist;
-if(!fs.existsSync(audioDir)) {
-    fs.mkdirSync(audioDir);
-    console.log(`Audio directory did not exist so one was created at: ${audioDir}\n`+ 
-    "Add the audioFiles you want available there.")
-} 
-const files = fs.readdirSync(audioDir).map(file => { return file.split('.')[0] })
+if (!existsSync(audioDir)) {
+  mkdirSync(audioDir);
+  console.log(
+    `Audio directory did not exist so one was created at: ${audioDir}\n` +
+      "Add the audioFiles you want available there.",
+  );
+}
+const files = readdirSync(audioDir).map((file) => {
+  return file.split(".")[0];
+});
 
-const { prefix, token } = require('./config.json');
+client.once(Events.ClientReady, async () => {
+  console.log("Ready!");
+  const connections = getVoiceConnections().values();
+  for (const connection of connections) {
+    connection.destroy();
+  }
+});
 
-client.once('ready', async () => console.log('Ready!'));
+client.on(Events.MessageCreate, async (message) => {
+  //If the message doesn't start with a prefix, is sent by a bot, or is a direct message.
+  if (
+    !message.content.startsWith(prefix) ||
+    message.author.bot ||
+    message.channel.type != ChannelType.GuildText
+  )
+    return;
 
-client.login(token);
+  //Get arguments after prefix
+  const args = message.cleanContent.slice(prefix.length).trim().split(/ +/);
 
-client.on('message', async message => {
-    
-    //If the message doesn't start with a prefix, is sent by a bot, or is a direct message.
-    if (!message.content.startsWith(prefix)
-        || message.author.bot
-        || message.channel.type != 'text') return;
+  //Get the first argument as command
+  const commandName = args.shift().toLowerCase();
+  const filePath = join(audioDir, commandName + ".mp3");
 
-    //Get arguments after prefix
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
+  // HELP: List available commands
+  // Or play the file with the same name as the command if it exists
+  if (commandName === "help") {
+    let msgStr = "***Available commands:***\n``" + files.join("\n") + "``";
+    message.channel.send(msgStr);
+  } else if (files.includes(commandName) && existsSync(filePath)) {
+    const channel = message.member.voice.channel;
+    play(filePath, channel, message);
+  }
+});
 
-    //Get the first argument as command
-    const commandName = args.shift().toLowerCase();
-    const filePath = path.join(audioDir, commandName + '.mp3')
-
-    // HELP: List available commands
-    // Or play the file with the same name as the command if it exists
-    if (commandName === 'help') {
-        let msgStr = "***Available commands:***\n``" + files.join("\n") + "``";
-        message.channel.send(msgStr);
-    }else if (files.includes(commandName) && fs.existsSync(filePath) && (!message.guild.voice || !message.guild.voice.connection)){
-        const vc = message.member.voice.channel;
-        play(filePath, vc);
-    }
-    
-})    
-
+client.login(TOKEN);
--- play.js
+++ play.js
@@ -1,39 +1,64 @@
-const Discord = require('discord.js')
-const path = require('path')
+import { createReadStream } from "fs";
+import { VoiceChannel, Message } from "discord.js";
+import {
+  AudioPlayer,
+  AudioPlayerStatus,
+  createAudioResource,
+  joinVoiceChannel,
+} from "@discordjs/voice";
+import { basename } from "path";
 
 /**
  * @param {string} fileName
- * @param {Discord.VoiceChannel} vc
+ * @param {VoiceChannel} channel
+ * @param {Message} message
  */
-const play = (fileName, vc) => {
-    if (vc) {
-        fileBaseName = path.basename(fileName);
-        try {     
-            vc.join().then(connection => {
-                const dispatcher = connection.play(fileName);
-    
-                dispatcher.on('start', () => {
-                    console.log(fileBaseName + ' is now playing!');
-                });
-    
-                dispatcher.on('finish', () => {
-                    console.log(fileBaseName + ' has finished playing!');
-                    vc.leave();
-                });
-    
-                // Always remember to handle errors appropriately!
-                dispatcher.on('error', (error) => {
-                    console.log(error);
-                });
-    
-            });
-        } catch (error) {
-            console.log(error)
-            vc.leave()
-        }
-    } else {
-        message.reply('You need to join a voice channel first!');
-    }
-}
+const play = (fileName, channel, message) => {
+  if (channel && channel.joinable) {
+    console.log("Channel guild", channel.guild);
+    const connection = joinVoiceChannel({
+      channelId: channel.id,
+      guildId: channel.guild.id,
+      adapterCreator: channel.guild.voiceAdapterCreator,
+    });
 
-module.exports = play;
+    try {
+      const player = new AudioPlayer();
+
+      const subscription = connection.subscribe(player);
+
+      if (!subscription) {
+        console.error("Failed to subscribe to audio player");
+        connection.destroy();
+        return;
+      }
+
+      setTimeout(() => subscription.unsubscribe(), 5_000);
+
+      const resource = createAudioResource(createReadStream(fileName));
+      const fileBaseName = basename(fileName);
+
+      player.play(resource);
+
+      player.on("error", () => {
+        console.error("FAILED TO PLAY\n", error);
+      });
+
+      player.on(AudioPlayerStatus.Playing, () => {
+        console.log(fileBaseName + " is now playing!");
+      });
+
+      player.on(AudioPlayerStatus.Idle, () => {
+        console.log(fileBaseName + " has finished playing!");
+        connection.destroy();
+      });
+    } catch (error) {
+      console.log(error);
+      connection.destroy();
+    }
+  } else {
+    message.reply("You need to join a voice channel first!");
+  }
+};
+
+export default play;
